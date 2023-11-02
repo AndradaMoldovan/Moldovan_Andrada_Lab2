d17cbdd7ed5b40731398554162516b4ebb281069
ANDRADA MOLDOVAN <andrada.moldovan1@stud.ubbcluj.ro>
2023-11-02 18:52:22 +0200

    Lab4

--- Models/Author.cs
+++ Models/Author.cs
@@ -1,9 +1,20 @@
-﻿namespace Moldovan_Andrada_Lab2.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Moldovan_Andrada_Lab2.Models
 {
     public class Author
     {
         public int ID { get; set; }
         public string AuthorName { get; set; }
         public ICollection<Book>? Book { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return AuthorName;
+            }
+        }
     }
 }
